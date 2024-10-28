6cd7d4214f979cc2a434b85900b7979ce2c1e415
MATIAS <PC@MATIAS>
2024-10-28 00:31:56 -0300

    Clase LibroNoEncontradoException MatiasBrotons

--- src/ar/edu/unju/escmi/poo/exceptions/LibroNoEncontradoException.java
+++ src/ar/edu/unju/escmi/poo/exceptions/LibroNoEncontradoException.java
@@ -1,5 +1,12 @@
 package ar.edu.unju.escmi.poo.exceptions;
 
-public class LibroNoEncontradoException {
+public class LibroNoEncontradoException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
+	public LibroNoEncontradoException(String mensaje) {
+        super(mensaje);
+    }
 }
