7b8475117796c6b8194446bf72f0814af44e1f9a
Sergio <PC@Sertin468>
2024-10-28 00:21:58 -0300

    Clase FechaUtil Completa

--- src/ar/edu/unju/escmi/poo/util/FechaUtil.java
+++ src/ar/edu/unju/escmi/poo/util/FechaUtil.java
@@ -1,5 +1,19 @@
 package ar.edu.unju.escmi.poo.util;
 
-public class FechaUtil {
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
+public class FechaUtil {
+	
+	public static LocalDate convertirStringLocalDate(String fechaStr) {
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		try {
+            return LocalDate.parse(fechaStr, formatter);
+        } catch (DateTimeParseException e) {
+            throw new DateTimeParseException("Error al convertir la fecha: " + fechaStr, fechaStr, e.getErrorIndex(), e);
+        }
+	}
 }
